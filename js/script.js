12985cfa3645fe0cd7bf60cf1490865e34f9c8da
GPierre62 <godissart@gmail.com>
2018-01-03 17:16:34 +0100

    test js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var min = 1;
 var max = 3;
 var scoreOrdinateur = Math.floor(Math.random()* (max - min + 1))+ min;
 console.log(scoreOrdinateur);
-document.getElementById("1").addEventListener("click",function(){ alert("t'as clické sur le 1!"); });
-document.getElementById("2").addEventListener("click",function(){ alert("t'as clické sur le 2!"); });
-document.getElementById("3").addEventListener("click",function(){ alert("t'as clické sur le 3!"); }); 
+window.onload = function(){
+   document.getElementById("1").addEventListener("click",function(){ alert("t'as clické sur le 1!");
+    });
+  }
