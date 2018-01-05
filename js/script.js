8529b1d5d63a7473a99908cff01d4c57b141a708
GPierre62 <godissart@gmail.com>
2018-01-05 11:37:29 +0100

    ajout aside aux autres pages

--- js/script.js
+++ js/script.js
@@ -5,17 +5,27 @@ var scoreOrdi = Math.floor(Math.random()* (max - min + 1))+ min;
 console.log(scoreOrdi);
 function clickUtilisateur(block){
   clickUtil=block.getAttribute('id');
+  //clickOrdi=document.getElementById(clickOrdi)
   if (clickUtil == scoreOrdi) {
     console.log("match nul !!");
 
   }else if(clickUtil==1 && scoreOrdi==3) {
     console.log("c'est gagné, c'est gagné !!");
-    document.getElementById("choixOrdi").document.write("<img src='../img/ciseaux.png'");
+    // document.getElementById("choixOrdi").document.write("<img src='../img/ciseaux.png'");
   }else if(clickUtil==2 && scoreOrdi==1) {
     console.log("c'est gagné, c'est gagné !!");
-    document.getElementById("choixOrdi").document.write("<img src='../img/pierre.png'");
+    // document.getElementById("choixOrdi").document.write("<img src='../img/pierre.png'");
   }else if(clickUtil==3 && scoreOrdi==2) {
     console.log("c'est gagné, c'est gagné !!");
-    document.getElementById("choixOrdi").document.write("<img src='../img/feuille.png'");
+    // document.getElementById("choixOrdi").document.write("<img src='../img/feuille.png'");
+  }else if(clickUtil==1 && scoreOrdi==2) {
+    console.log("c'est perdu, c'est perdu !!");
+    // document.getElementById("choixOrdi").document.write("<img src='../img/feuille.png'");
+  }else if(clickUtil==2 && scoreOrdi==3) {
+    console.log("c'est perdu, c'est perdu !!");
+    // document.getElementById("choixOrdi").document.write("<img src='../img/feuille.png'");
+  }else if(clickUtil==3 && scoreOrdi==1) {
+    console.log("c'est perdu, c'est perdu !!");
+    // document.getElementById("choixOrdi").document.write("<img src='../img/feuille.png'");
   }
 }
