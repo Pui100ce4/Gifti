19457d6f02f66ba3022291ff5f1a1af27e7ad63b
Heleneb62 <helenebelvalpro@gmail.com>
2018-01-07 17:32:33 +0100

    ajout de la fleche top sur l'index page fille et moins de 5 ans

--- js/banniere.js
+++ js/banniere.js
@@ -30,3 +30,31 @@ function defilimage(){
     slideIndex += 1;
     showSlides(slideIndex);
 }
+window.onscroll = function (ev) {
+
+   var footer = document.querySelector('footer');
+    var moveup = null;
+    if (document.querySelector('.moveup') === null) {
+        moveup = document.createElement('div');
+        moveup.className = 'moveup';
+        var fleche = document.createElement('span');
+        fleche.className = 'fa fa-angle-up fleche';
+        fleche.onclick = topFunction();
+        moveup.appendChild(fleche);
+        footer.insertAdjacentElement('afterend', moveup);
+   } else {
+        moveup = document.querySelector('.moveup');
+    }
+
+   var header = document.querySelector('header');
+    if ((Math.ceil(window.pageYOffset)) > header.offsetHeight) {
+        moveup.style.display = 'block';
+    } else {
+        moveup.style.display = 'none';
+    }
+};
+
+function topFunction() {
+   // document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
