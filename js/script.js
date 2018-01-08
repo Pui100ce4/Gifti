660a4dfe07ba1e257d0d32e4d6e4398cf2502745
GPierre62 <godissart@gmail.com>
2018-01-08 13:41:47 +0100

    push partie mini-jeux

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-
 var min = 1;
 var max = 3;
 var scoreOrdi = Math.floor(Math.random()* (max - min + 1))+ min;
 console.log(scoreOrdi);
 function clickUtilisateur(block){
   clickUtil=block.getAttribute('id');
-  //clickOrdi=document.getElementById(clickOrdi)
+  clickOrdi=document.getElementById("choixOrdi");
+
+  // console.log(clickOrdi)
   if (clickUtil == scoreOrdi) {
     console.log("match nul !!");
 
   }else if(clickUtil==1 && scoreOrdi==3) {
     console.log("c'est gagné, c'est gagné !!");
-    // document.getElementById("choixOrdi").document.write("<img src='../img/ciseaux.png'");
+    clickOrdi.document.write('<div><img src="../img/pierre.png" style="height: 20%;width: 20%;"><img src="../img/ciseaux.png"style="height: 20%;width: 20%;"></div>');
   }else if(clickUtil==2 && scoreOrdi==1) {
     console.log("c'est gagné, c'est gagné !!");
     // document.getElementById("choixOrdi").document.write("<img src='../img/pierre.png'");
